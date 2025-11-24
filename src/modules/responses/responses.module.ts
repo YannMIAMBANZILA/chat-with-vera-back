27d7f828dbfb58aadd8d41baaf169fcd11aa1ac4
KerdanetYvan <kerdanety@gmail.com>
2025-11-24 20:52:48 +0100

    Refactor import statements and formatting in ResponsesModule for consistency

--- src/modules/responses/responses.module.ts
+++ src/modules/responses/responses.module.ts
@@ -1,14 +1,19 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ResponsesService } from "./responses.service";
-import { ResponsesController } from "./responses.controller";
-import { Response, ResponseSchema } from "./dto/schemas/responses.schema";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ResponsesService } from './responses.service';
+import { ResponsesController } from './responses.controller';
+import { Response, ResponseSchema } from './dto/schemas/responses.schema';
 
 @Module({
-    controllers: [ResponsesController], // enregistrer le contrôleur des réponses
-    providers: [ResponsesService], // enregistrer le service des réponses
-    imports: [
-        MongooseModule.forFeature([{ name: Response.name, schema: ResponseSchema }]), // enregistrer le schéma Mongoose pour les réponses
-    ],
+  controllers: [ResponsesController], // enregistrer le contrôleur des réponses
+  providers: [ResponsesService], // enregistrer le service des réponses
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Response.name,
+        schema: ResponseSchema,
+      },
+    ]), // enregistrer le schéma Mongoose pour les réponses
+  ],
 })
 export class ResponsesModule {}
